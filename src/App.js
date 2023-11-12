70057801c99ae3c88e3191638e6cbf6d1bc3d04b
Prabhu67 <p.prabhu.pb@gmail.com>
2023-11-12 22:40:32 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  const [amount, setamount] = useState('');
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(amount === ""){
+    alert("please enter amount");
+    }else{
+      var options = {
+        key: "rzp_test_cbKx98aNvRvQ2e",
+        key_secret:"cyCIPwE637LMQNpIQEIUgzqO",
+        amount: amount *100,
+        currency:"INR",
+        name:"STARTUP_PROJECTS",
+        description:"for testing purpose",
+        handler: function(response){
+          alert(response.razorpay_payment_id);
+        },
+        prefill: {
+          name:"",
+          email:"",
+          contact:""
+        },
+        notes:{
+          address:"Razorpay Corporate office"
+        },
+        theme: {
+          color:"#3399cc"
+        }
+      };
+      var pay = new window.Razorpay(options);
+      pay.open();
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Razorpay Payment </h2>
+     <br/>
+     <input type="text"placeholder='Enter Amount'value={amount}onChange={(e)=>setamount(e.target.value)} />
+     <br/><br/>
+     <button onClick={handleSubmit}>submit</button>
     </div>
   );
 }
